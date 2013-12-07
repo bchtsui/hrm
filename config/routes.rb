073b38ad64ae7e399002699d100f8c0c252fd418
bryan <bchtsui@hotmail.com>
2013-12-07 23:13:53 +0800

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Hrm::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  
   resources :rank_histories
-
   resources :members
 
   # The priority is based upon order of creation: first created -> highest priority.
